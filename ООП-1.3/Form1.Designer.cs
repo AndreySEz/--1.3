fd54c4e212a819e692b1eb143b41f4fce0cd4c1e
Andreus <Andreus@DESKTOP-I3EQQ7H>
2020-06-19 13:44:19 +0500

    Добавьте файлы проекта.

---
+++ ООП-1.3/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ООП_1._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxX1 = new System.Windows.Forms.TextBox();
+            this.labelX1 = new System.Windows.Forms.Label();
+            this.textBoxY1 = new System.Windows.Forms.TextBox();
+            this.labelY1 = new System.Windows.Forms.Label();
+            this.textBoxX2 = new System.Windows.Forms.TextBox();
+            this.labelX2 = new System.Windows.Forms.Label();
+            this.textBoxY2 = new System.Windows.Forms.TextBox();
+            this.labelY2 = new System.Windows.Forms.Label();
+            this.textBoxX3 = new System.Windows.Forms.TextBox();
+            this.labelX3 = new System.Windows.Forms.Label();
+            this.textBoxY3 = new System.Windows.Forms.TextBox();
+            this.labelY3 = new System.Windows.Forms.Label();
+            this.labelOut = new System.Windows.Forms.Label();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxX1
+            // 
+            this.textBoxX1.Location = new System.Drawing.Point(109, 11);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX1.TabIndex = 0;
+            // 
+            // labelX1
+            // 
+            this.labelX1.AutoSize = true;
+            this.labelX1.Location = new System.Drawing.Point(14, 14);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(89, 13);
+            this.labelX1.TabIndex = 1;
+            this.labelX1.Text = "Корордината X1";
+            // 
+            // textBoxY1
+            // 
+            this.textBoxY1.Location = new System.Drawing.Point(326, 11);
+            this.textBoxY1.Name = "textBoxY1";
+            this.textBoxY1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY1.TabIndex = 0;
+            // 
+            // labelY1
+            // 
+            this.labelY1.AutoSize = true;
+            this.labelY1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.labelY1.Location = new System.Drawing.Point(231, 14);
+            this.labelY1.Name = "labelY1";
+            this.labelY1.Size = new System.Drawing.Size(89, 13);
+            this.labelY1.TabIndex = 1;
+            this.labelY1.Text = "Корордината Y1";
+            // 
+            // textBoxX2
+            // 
+            this.textBoxX2.Location = new System.Drawing.Point(109, 44);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX2.TabIndex = 0;
+            // 
+            // labelX2
+            // 
+            this.labelX2.AutoSize = true;
+            this.labelX2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.labelX2.Location = new System.Drawing.Point(14, 47);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(89, 13);
+            this.labelX2.TabIndex = 1;
+            this.labelX2.Text = "Корордината X2";
+            // 
+            // textBoxY2
+            // 
+            this.textBoxY2.Location = new System.Drawing.Point(326, 44);
+            this.textBoxY2.Name = "textBoxY2";
+            this.textBoxY2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY2.TabIndex = 0;
+            // 
+            // labelY2
+            // 
+            this.labelY2.AutoSize = true;
+            this.labelY2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.labelY2.Location = new System.Drawing.Point(231, 47);
+            this.labelY2.Name = "labelY2";
+            this.labelY2.Size = new System.Drawing.Size(89, 13);
+            this.labelY2.TabIndex = 1;
+            this.labelY2.Text = "Корордината Y2";
+            // 
+            // textBoxX3
+            // 
+            this.textBoxX3.Location = new System.Drawing.Point(110, 76);
+            this.textBoxX3.Name = "textBoxX3";
+            this.textBoxX3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX3.TabIndex = 0;
+            // 
+            // labelX3
+            // 
+            this.labelX3.AutoSize = true;
+            this.labelX3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.labelX3.Location = new System.Drawing.Point(15, 79);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(89, 13);
+            this.labelX3.TabIndex = 1;
+            this.labelX3.Text = "Корордината X3";
+            // 
+            // textBoxY3
+            // 
+            this.textBoxY3.Location = new System.Drawing.Point(327, 76);
+            this.textBoxY3.Name = "textBoxY3";
+            this.textBoxY3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY3.TabIndex = 0;
+            // 
+            // labelY3
+            // 
+            this.labelY3.AutoSize = true;
+            this.labelY3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.labelY3.Location = new System.Drawing.Point(232, 79);
+            this.labelY3.Name = "labelY3";
+            this.labelY3.Size = new System.Drawing.Size(89, 13);
+            this.labelY3.TabIndex = 1;
+            this.labelY3.Text = "Корордината Y3";
+            // 
+            // labelOut
+            // 
+            this.labelOut.AutoSize = true;
+            this.labelOut.Location = new System.Drawing.Point(15, 143);
+            this.labelOut.Name = "labelOut";
+            this.labelOut.Size = new System.Drawing.Size(0, 13);
+            this.labelOut.TabIndex = 2;
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(133, 111);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(163, 23);
+            this.buttonTest.TabIndex = 3;
+            this.buttonTest.Text = "Проверка на правильность";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 204);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.labelOut);
+            this.Controls.Add(this.labelY3);
+            this.Controls.Add(this.labelY2);
+            this.Controls.Add(this.labelY1);
+            this.Controls.Add(this.textBoxY3);
+            this.Controls.Add(this.textBoxY2);
+            this.Controls.Add(this.textBoxY1);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.textBoxX3);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.textBoxX1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelX1;
+        private System.Windows.Forms.TextBox textBoxY1;
+        private System.Windows.Forms.Label labelY1;
+        private System.Windows.Forms.TextBox textBoxX2;
+        private System.Windows.Forms.Label labelX2;
+        private System.Windows.Forms.TextBox textBoxY2;
+        private System.Windows.Forms.Label labelY2;
+        private System.Windows.Forms.TextBox textBoxX3;
+        private System.Windows.Forms.Label labelX3;
+        private System.Windows.Forms.TextBox textBoxY3;
+        private System.Windows.Forms.Label labelY3;
+        private System.Windows.Forms.Label labelOut;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.TextBox textBoxX1;
+    }
+}
+
---
+++ ООП-1.3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ООП_1._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Triangle
+        {
+            public double a, b, c;
+            public double Alpha, Betta, Gamma;
+
+            public void LinesLength(double x1, double x2, double x3, double y1, double y2, double y3)
+            {
+                a = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                b = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+                c = Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
+            }
+            public bool testTriangle()
+            {
+                if (((a + b > c) && (b + c > a) && (c + a > b)) == false)
+                    return false;
+                else
+                    return true;
+            }
+            public void AnglesTriangle()
+            {
+                Alpha = Math.Cos((b * b + c * c - a * a) / (2 * b * c))*180/Math.PI;
+                Betta = Math.Cos((a * a + c * c - b * b) / (2 * a * c)) * 180 / Math.PI;
+                Gamma = Math.Cos((a * a + b * b - c * c) / (2 * a * b)) * 180 / Math.PI;
+            }
+        }
+
+
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            double x1= Convert.ToDouble(textBoxX1.Text);
+            double x2 = Convert.ToDouble(textBoxX2.Text);
+            double x3 = Convert.ToDouble(textBoxX3.Text);
+            double y1 = Convert.ToDouble(textBoxY1.Text);
+            double y2 = Convert.ToDouble(textBoxY2.Text);
+            double y3 = Convert.ToDouble(textBoxY3.Text);
+            Triangle triangleMain = new Triangle();
+            triangleMain.LinesLength(x1, x2, x3, y1, y2, y3);
+            triangleMain.AnglesTriangle();
+            if (triangleMain.testTriangle()) labelOut.Text = $"Треугольник возможен. Его стороны: {Math.Round(triangleMain.a, 2)}," +
+                    $"{Math.Round(triangleMain.b, 2)}, {Math.Round(triangleMain.c, 2)}. \n Его углы: {triangleMain.Alpha}, {triangleMain.Betta}, {triangleMain.Gamma}";
+            else labelOut.Text = "Треугольник невозможен";
+        }
+    }
+}
